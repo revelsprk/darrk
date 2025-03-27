6c7c394e9474ae411b893dd8b49e758a9c01b777
shanghai-romance-0210 <shanghai.romance.0210@gmail.com>
2025-03-27 23:48:20 +0900

    Update

--- src/app/[roomId]/page.tsx
+++ src/app/[roomId]/page.tsx
@@ -295,7 +295,7 @@ export default function RoomPage() {
 
       {/* Header */}
       <div className="px-8 py-4 flex items-center select-none h-16 bg-white z-50 sticky top-0 shadow-md">
-        <Link href="/"><Image src="/mediakit/logotype.svg" alt="Logo" width={100} height={100} className="h-6 min-h-[24px] w-fit" /></Link>
+        <div><Link href="/"><Image src="/mediakit/logotype.svg" alt="Logo" width={100} height={100} className="h-6 w-fit" /></Link></div>
         <div className="ml-auto relative z-10">
           <Button variant="outline" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>設定</Button>
           <div ref={dropdownRef} className={`absolute right-0 mt-2 w-64 bg-white rounded-md special-shadow p-2 transition-all duration-200 ease-in-out ${ isDropdownOpen ? "scale-100 opacity-100" : "scale-95 opacity-0 pointer-events-none"}`}>
