761c0f8f59a9fdfd178a506765c702e8bce63051
shanghai-romance-0210 <shanghai.romance.0210@gmail.com>
2025-03-28 11:09:18 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex items-center justify-center select-none h-16 bg-white sticky top-0 z-50 shadow-md">
-        <Link href="/"><div className="h-full flex items-center w-full"><Image src="/mediakit/logotype.svg" alt="Logo" width={100} height={100} className="h-6 w-fit" /></div></Link>
+        <Link href="/"><Image src="/mediakit/logotype.svg" alt="Logo" width={24} height={24} className="h-6 w-fit" /></Link>
       </div>
 
       <div className="md:container mx-auto px-4 md:px-0 py-8">
